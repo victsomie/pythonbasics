43d1e8bd9f74364c5f720f10f903af3cced055fa
victsomie <victsomie@yahoo.com>
2016-04-27 12:33:15 +0300

    added more list fucntions

--- lists.py
+++ lists.py
@@ -3,3 +3,7 @@ my_list = [1, 2, 3 "victor", 4.5]
 print my_list[0]  #prints the first item in the list
 print append ("James") # This adds James to the end of the list
 print len(my_list)   #Prints the length of the list
+print my_list[0:4]  #Prints index 0 up to but excluding index 4
+print my_list[0:len(my_list)] #Prints index 0 up to but excluding the last item
+print my_list[0:]  #This prints from the first to the last item
+
