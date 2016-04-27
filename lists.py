53979ed92ae5b54cc69a2abfb298c6de5344d449
victsomie <victsomie@yahoo.com>
2016-04-27 11:38:46 +0300

    added more list functions

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
 my_list = [1, 2, 3 "victor", 4.5]
 
 print my_list[0]  #prints the first item in the list
+print append ("James") # This adds James to the end of the list
+print len(my_list)   #Prints the length of the list
