4b7cb860ed279b497599064dfdc50f1a82be9a86
victsomie <victsomie@yahoo.com>
2016-04-27 09:59:58 +0300

    added content to learn

--- built_in_functions.py
+++ built_in_functions.py
@@ -8,3 +8,18 @@ min(2, 3, 4) # 2
 
 abs(2) # 2
 abs(-2) # 2
+
+
+"""
+First, def a function called distance_from_zero, with one argument (choose any argument name you like).
+If the type of the argument is either int or float, the function should return the absolute value of the function input.
+Otherwise, the function should return "Nope"
+
+"""
+
+# Sample code
+def distance_from_zero(s):
+    if type(s)==int or type(s) ==float:
+        return abs(s)
+    else:
+        return "Nope"
