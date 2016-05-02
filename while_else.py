a3889f41bc2ea5d6e14d858a159b8133b3bd0e66
victsomie <victsomie@yahoo.com>
2016-05-02 18:17:44 +0300

    added the rest of the while/else body

--- while_else.py
+++ while_else.py
@@ -1,1 +1,10 @@
 def add(num):
+    while num<20:
+        print num
+        num+=1
+        if num==17:
+            break
+    else:  #This doesnt execute if the break statement is reached
+        print "Numbers fro here are greater the one given in the condition"
+
+add(10)
