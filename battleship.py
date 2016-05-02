f7dea76ab16933f745e00c7f67974904e06eca6e
victsomie <victsomie@yahoo.com>
2016-05-02 15:22:02 +0300

    created list and appended list into another list (board)

---
+++ battleship.py
@@ -0,0 +1,8 @@
+from random import randint #Import randint from random to help in generating random numbers
+
+#Creaate a empty list
+board = []
+
+#Append 5 lists (with "0") into board
+for x in range(0, 5):
+    board.append(["O"] * 5)
