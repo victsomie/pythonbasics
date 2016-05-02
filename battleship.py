0851f0309c6b4ba6c3a138269b8362720f4c930a
victsomie <victsomie@yahoo.com>
2016-05-02 16:22:03 +0300

    added a for loop to prompt playing four times

--- battleship.py
+++ battleship.py
@@ -1,22 +1,17 @@
-from random import randint #Import randint from random to help in generating random numbers
+from random import randint
 
-#Creaate a empty list
 board = []
 
-#Append 5 lists (with "0") into board
-for x in range(0, 5):
+for x in range(5):
     board.append(["O"] * 5)
 
-#Iterates through all lists joining the elements using a space
 def print_board(board):
     for row in board:
         print " ".join(row)
 
-#First instance off printing the board so far
+print "Let's play Battleship!"
 print_board(board)
 
-#Makes guess of ship_row number and ship_col number
-#Prompt the input of the user to input numbers for the col and row
 def random_row(board):
     return randint(0, len(board) - 1)
 
@@ -25,28 +20,26 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
-
-
-#Print the randomised column and row number
-#Check
-
 print ship_row
 print ship_col
 
-# Write your code below!
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
-else:
-    if guess_row not in range(0, len(board)) or guess_col not in range(0, len(board[0])):
-        print "Oops, that's not even in the ocean."
-    elif board[guess_row][guess_col]=="X": #Checks for repetation
-        print "You guessed that one already."
+# Everything from here on should go in your for loop!
+# Be sure to indent four spaces!
+turn = 0
+for d in range(0,4):
+    guess_row = int(raw_input("Guess Row:"))
+    guess_col = int(raw_input("Guess Col:"))
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
     else:
-        print "You missed my battleship!"
-
-#Append letter "X" to the user's guess numbers to positon and print it
-board[guess_row][guess_col] = "X"
-
-print board
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X"
+    # Print (turn + 1) here!
+    turn = turn + 1
+    print "%s" %(turn+1)
+    print_board(board)
