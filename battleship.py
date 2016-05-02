3ce7ff1ce1a7d7ecadc7ff1b2125899287c4213b
victsomie <victsomie@yahoo.com>
2016-05-02 15:25:52 +0300

    added print function spacing all list elements with a space (" ")

--- battleship.py
+++ battleship.py
@@ -6,3 +6,8 @@ board = []
 #Append 5 lists (with "0") into board
 for x in range(0, 5):
     board.append(["O"] * 5)
+
+#Iterates through all lists joining the elements using a space
+def print_board(board):
+    for row in board:
+        print " ".join(row)
