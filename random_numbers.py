ad12601f2943cefbac1cd99c2aaf7d9c12d561cf
victsomie <victsomie@yahoo.com>
2016-05-02 12:29:14 +0300

    Added comments to lines

--- random_numbers.py
+++ random_numbers.py
@@ -1,5 +1,6 @@
-from random import randint
+from random import randint #Import the module for containing random numbers
 
-x = randint(0, 20)
+
+x = randint(0, 20) #Assigns variable x to a random number betwween 0 and 20
 
 print x
