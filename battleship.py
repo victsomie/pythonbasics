c0a8b2e1204e179da2b73cfe4762c1a94f814032
victsomie <victsomie@yahoo.com>
2016-05-02 15:30:19 +0300

    added function for creating random row and function

--- battleship.py
+++ battleship.py
@@ -13,3 +13,16 @@ def print_board(board):
         print " ".join(row)
 
 #First instance off printing the board so far
+print_board(board)
+
+#Makes guess of random row and column and assign them to ship_row and ship_col
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+
