d64b151d02b4ddcef9685c1fbee15bd6dc0c4d8c
victsomie <victsomie@yahoo.com>
2016-05-02 16:47:17 +0300

    Added variable win

--- battleship.py
+++ battleship.py
@@ -25,12 +25,14 @@ print ship_col
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
+win = 0
 turn = 0
 for d in range(0,4):
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+        win = win +1
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
@@ -39,10 +41,9 @@ for d in range(0,4):
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-    #Check if turn have reached 3 and print out "Game Over"
     if turn==3:
         print "Game Over"
-    elif win>2:
+    if win>2:
         break
     # Print (turn + 1) here!
     turn = turn + 1
