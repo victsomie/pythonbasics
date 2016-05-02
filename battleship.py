065d1ef675b0b46727161322475b39dc0f229814
victsomie <victsomie@yahoo.com>
2016-05-02 16:49:19 +0300

    Added comments on line

--- battleship.py
+++ battleship.py
@@ -25,14 +25,14 @@ print ship_col
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
-win = 0
-turn = 0
+win = 0 #Initializes variable win to count number of wins
+turn = 0 #Initializes variable turn to count the turn number
 for d in range(0,4):
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
-        win = win +1
+        win = win +1 #Increaments variable 1 when ship is sunk
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
