cd1760afebe36f4484900ce91d4106d43e0bd55d
victsomie <victsomie@yahoo.com>
2016-05-03 04:27:10 +0300

    added enumerate function

--- for_loop.py
+++ for_loop.py
@@ -1,2 +1,14 @@
+
+#This iterates five times printing athe string on each iteration
 for x in range(5):
     print "am learning the for loops"
+
+
+"""
+enumerate function
+==================
+"""
+l = ["james", "john", "cate", "dan"] #declare a variable list
+
+for index, item in enumerate(l):
+    print index+1 + " " + item  #Loops and prints index number from 1 and item in the list
