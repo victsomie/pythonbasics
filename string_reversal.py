1b2c2bbe0a164851eb60de774d1d0dc10f128fb7
victsomie <victsomie@yahoo.com>
2016-05-05 19:56:21 +0300

    added commenting to the code

--- string_reversal.py
+++ string_reversal.py
@@ -1,6 +1,7 @@
 """A function to reverse a string"""
 def  reverse(text):
     s = ""
+    #Loop over the list writting characters inside
     for x in range(0, len(text)):
-        s = text[x] + s
-    return s
+        s = text[x] + s #Add to the character behind the string
+    return s #Returns the string created
