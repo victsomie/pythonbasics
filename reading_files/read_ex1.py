2928fa1fcaaa89629f79700fb978e0f0f21c9871
victsomie <victsomie@yahoo.com>
2016-05-15 22:11:39 +0300

    added content to prompt user yto enter a parameter

--- reading_files/read_ex1.py
+++ reading_files/read_ex1.py
@@ -11,3 +11,11 @@ txt = open(filename) #Declares a variable that opens the file and the parameteri
 
 print "Here's your file %r:" % filename #This gets the file name and print its out
 print txt.read() #This prints out the txt variable, which was the file given
+
+
+print "Type the filename again:"
+file_again = raw_input("> ") #Prompts the user for any file name
+
+txt_again = open(file_again) #Creates a variable that open that file given as a parameter
+
+print txt_again.read() #Prints  the filename that has been requested through parameter
