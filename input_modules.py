779c8a6362d78c32ed2ffe46f73b940834516495
victsomie <victsomie@yahoo.com>
2016-05-15 21:55:00 +0300

    added more content

--- input_modules.py
+++ input_modules.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 """
-Take whatever is in argv, unpack it, and assign it to all these variables on the left in order.
 
 
 To run the program you must pass three command line arguments):
@@ -8,7 +7,19 @@ To run the program you must pass three command line arguments):
 
 from sys import argv
 
-script, first, second, third = argv
+"""
+first = raw_input("First entry: ")
+second = raw_input("Second entry: ")
+third = raw_input("Third entry: ")
+"""
+
+script, first, second, third = argv #Take whatever is in argv, unpack it, and assign it to all these variables on the left in order.
+
+
+prompt users response
+first = raw_input("First entry: ")
+second = raw_input("Second entry: ")
+third = raw_input("Third entry: ")
 
 print "The script is called:", script
 print "Your first variable is:", first
