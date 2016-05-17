8f908fe10684dac9bcf0d26afdac32d8e8d317c0
victsomie <victsomie@yahoo.com>
2016-05-17 06:34:31 +0300

    added sets content

---
+++ sets.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+A program to demonstrate SETS and manipulation
+
+A set is a an unordered collection with no duplicates
+it is written with braces
+
+my_set = ( <CONTENT HERE> )
+"""
+
+
+a = (1, 3, 5, 6, 3, 2)
+
+numbers = set(a)
+print a
+print numbers
