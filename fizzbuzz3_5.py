b927326f1ef1a1b0e9dd45de33a452c297ba939d
victsomie <victsomie@yahoo.com>
2016-05-26 06:18:22 +0300

    added fizzbuzz test

--- fizzbuzz3_5.py
+++ fizzbuzz3_5.py
@@ -1,3 +1,9 @@
 def num(x):
     if x%3 == 0 and x%5 ==0:
-        
+         print "FIZZBUZZ!"
+    elif x%3 ==0 and x%5!=0:
+        print "FIZZ"
+    elif x%3!=0 and x%5==0:
+        print "BUZZ"
+
+num(18)
