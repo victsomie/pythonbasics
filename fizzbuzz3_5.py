e1d2d19e57e1e633c4c93dfd4cdb4e1a35f16546
victsomie <victsomie@yahoo.com>
2016-05-26 06:20:36 +0300

    added code to prompt user's input for number test

--- fizzbuzz3_5.py
+++ fizzbuzz3_5.py
@@ -1,9 +1,9 @@
 def num(x):
     if x%3 == 0 and x%5 ==0:
-         print "FIZZBUZZ!"
+        print "FIZZBUZZ!"
     elif x%3 ==0 and x%5!=0:
         print "FIZZ"
     elif x%3!=0 and x%5==0:
         print "BUZZ"
 
-num(18)
+num(int(raw_input("Enter number: ")))
