1307d0e2dfed688e6e01552cefb62cc81aae934f
victsomie <victsomie@yahoo.com>
2016-08-06 19:39:13 +0300

    added new file

--- Learn The Hard Way/ex1.py
+++ Learn The Hard Way/ex1.py
@@ -4,4 +4,11 @@ print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+#print 'I "said" do not touch this.'
+
+
+print "Hens: ", 25 +30 / 6
+print "Divide: 5 / 6 = ", 5/6
+print "Modulus: 6 % 2 = ", 6%4
+print "Less than: 5 < 6", 5 < 6
+print "Greater than: 10 > 6 = ", 10>6 
